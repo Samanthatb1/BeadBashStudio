1a303e8e127d0f269485c33c4ec3b495e00f23a0
Samantha Grieco <samtb1@outlook.com>
2023-03-27 21:30:51 -0400

    more cors

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const axios = require('axios').default;
 const path = require('path');
+const cors = require('cors')
 
 const PORT = process.env.PORT || 3001 ;
 
