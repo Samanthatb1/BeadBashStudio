50b7a66c31f4b980a144e97ae939e75cf8884e31
Samantha Grieco <samtb1@outlook.com>
2024-10-11 23:28:06 -0400

    qf

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const axios = require('axios').default;
 const path = require('path');
 const fs = require('fs');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3001 ;
 
