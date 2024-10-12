31c6b539b5a1b178759e78cde57e3c0540639bd8
Samantha Grieco <samtb1@outlook.com>
2024-10-11 23:02:38 -0400

    remove base route

--- server/index.js
+++ server/index.js
@@ -32,9 +32,9 @@ app.get("/allListings", async (req, res) => {
 });
 
 // All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+// });
 
 app.listen(PORT, () => {
   console.log("Server running on Port: " + PORT);
